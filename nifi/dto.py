904355f95f99973984c89b675a79dfdf9d9ac8cd
yonatan957 <yonatan22660@gmail.com>
2025-06-23 19:19:51 +0300

    add create funnel function

--- nifi/dto.py
+++ nifi/dto.py
@@ -1,3 +1,4 @@
+from enum import Enum
 def create_pg_payload(name:str, x_position:float= 0.0, y_position:float= 0.0) -> json :
     return {
         "revision": {"version":0},
@@ -6,3 +7,9 @@ def create_pg_payload(name:str, x_position:float= 0.0, y_position:float= 0.0) ->
             "position": {x_position, y_position}
         }
     }
+
+class Request_Type(Enum):
+    GET = "GET"
+    POST = "POST"
+    DELETE = "DELETE"
+    PUT = "PUT"
