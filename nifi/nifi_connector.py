15b62bac990b9cc6798c13e3e1e9f2a80e724721
yonatan957 <yonatan22660@gmail.com>
2025-06-23 16:30:37 +0300

    add typing to functions

--- nifi/nifi_connector.py
+++ nifi/nifi_connector.py
@@ -1,11 +1,12 @@
 import os
 import requests
+from typing import Dict
 
 NIFI_API_BASE = os.getenv("NIFI_API_BASE", "https://localhost:8443/nifi-api")
 USER_NAME = os.getenv("USER_NAME")
 PASSWORD = os.getenv("PASSWORD")
 
-def get_token():
+def get_token() -> str:
     res = requests.post(f'{NIFI_API_BASE}/access/token', data={
         "username": USER_NAME,
         "password": PASSWORD
@@ -14,7 +15,7 @@ def get_token():
         raise Exception(f"Failed to get token: {res.status_code} {res.text}")
     return res.text.strip()
 
-def check_nifi_connection():
+def check_nifi_connection() -> Dict[str, str]:
     try:
         token = get_token()
         ### a random request, just to check
