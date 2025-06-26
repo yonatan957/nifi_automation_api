2e6b658f63058e49d6f88fbb09442d592de65417
yonatan957 <yonatan22660@gmail.com>
2025-06-26 21:21:43 +0300

    create route for creating process group and ports

--- controllers/process_group.py
+++ controllers/process_group.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, current_app, request
-from nifi_objects.general_objects import ProcessGroup
+from nifi_objects.general_objects import ProcessGroup, ProcessGroupWithPorts
 from nifi_services.nifi_service import NifiService
 from controllers.controllers_utils import validated_payload
 
@@ -18,10 +18,17 @@ def get_root_id():
 
 @process_group_bp.route("/<father_id>", methods=["POST"])
 def create_process_group(father_id: str):
-    process_group = validated_payload(request, ProcessGroup, 'invalid input on create-process-group')
     nifi_service = current_app.config['nifi_service']
+    process_group = validated_payload(request, ProcessGroup, 'invalid input on create-process-group')
     new_process_group = nifi_service.create_process_group(process_group, father_id)
     return jsonify(new_process_group)
+
+@process_group_bp.route("/<father_id>/process-with-ports", methods=["POST"])
+def create_process_group_with_ports(father_id: str):
+    nifi_service = current_app.config['nifi_service']
+    process_group_with_ports = validated_payload(request, ProcessGroupWithPorts, 'invalid input on create-process-group')
+    new_process_group_with_ports = nifi_service.create_pg_with_ports(process_group_with_ports, father_id)
+    return jsonify(new_process_group_with_ports)
 
 @process_group_bp.route('/<father_id>', methods=['PUT'])
 def update_process_group(father_id: str):
