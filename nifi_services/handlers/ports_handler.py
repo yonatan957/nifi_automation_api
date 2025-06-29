12e927d912013cf09d0b2e1c441f59a1f694766d
yonatan957 <yonatan22660@gmail.com>
2025-06-29 15:36:32 +0300

    add function in handler to get all ports

--- nifi_services/handlers/ports_handler.py
+++ nifi_services/handlers/ports_handler.py
@@ -1,5 +1,6 @@
 from nifi_services.types import GenericDict, Request_Type
 from nifi_objects.general_objects import Port, InputPort, OutPutPort
+from nifi_services.types import PortType
 class PortsHandler:
     def __init__(self, nifi_request, validate_response_status):
         self.nifi_request = nifi_request
@@ -13,4 +14,13 @@ class PortsHandler:
             json=port.dict())
 
         self.validate_response_status(response, {200, 201}, 'failed to create port')
+        return response.json()
+
+    def get_all_ports(self, port_type: PortType, father_id:str):
+        end_of_url = 'input-ports' if port_type == PortType.INPUT_PORT else 'output-ports'
+        response = self.nifi_request(
+            Request_Type.GET,
+            f'/process-groups/{father_id}/{end_of_url}'
+        )
+        self.validate_response_status(response, {200, 201}, 'failed to get all ports')
         return response.json()
