e8e39d6dc25af91d2678d5614878a523b28384b4
yonatan957 <yonatan22660@gmail.com>
2025-06-29 15:33:42 +0300

    use connection type in controller

--- controllers/connection.py
+++ controllers/connection.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, jsonify, current_app, request
+from controllers.controllers_utils import validated_payload
+from nifi_objects.general_objects import Connection
 
 connection_bp = Blueprint('connections', __name__, url_prefix='/connections')
 
-@connection_bp.route('', methods=['POST'])
-def create_connection():
+@connection_bp.route('<father_id>', methods=['POST'])
+def create_connection(father_id:str):
     nifi_service = current_app.config['nifi_service']
-    service = not nifi_service
-    return jsonify(service)
+    connection = validated_payload(request, Connection, 'failed to create connection')
+    new_connection = nifi_service.create_connection(connection, father_id)
+    return jsonify(new_connection)
